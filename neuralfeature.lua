fbed8583bbd170c985261fbac1fefcf98665b022
Tejas Khot <tjskhot@gmail.com>
2015-07-04 17:55:13 +0800

    extracting features working

---
+++ neuralfeature.lua
@@ -0,0 +1,75 @@
+--[[
+Author: Yuhuang Hu
+Contact: duguyue100@gmail.com
+
+Note: This file provide a set of tools of extracting features
+]]
+
+local image=require 'image';
+
+neuralfeature={};
+
+---The function extracts features from choosen layer
+--
+-- Parameters
+-- ----------
+-- net_model : object
+--     a neural network model
+-- image_adds : table
+--     a list of path of images
+-- layer_id : integer
+--     chosen layer of the network 
+--
+-- Returns
+-- -------
+-- out_feature : table, elements are floatTensor
+--     list of extracted features
+-- out_labels : table
+--	   list of predicted labels by pre-trained model
+--
+function neuralfeature.extract(net_model, image_adds, layer_id)
+
+    -- default working with last fc layer
+    local layer_id = layer_id or #net.modules-1;
+    
+    out_feature={};
+    out_labels={};
+    
+    for i=1, #image_adds do
+        I=image.load(image_adds[i]);
+        _, y=net:forward(I:float():cuda()):max(1);
+        I_out=net.modules[layer_id].output;
+        
+        --print(I_out:float()[{ {1,10} }])
+        table.insert(out_feature, I_out:float());
+        table.insert(out_labels, y);
+    end
+    	
+    return out_feature, out_labels;
+end
+
+--- Load a list of image paths given a text file
+--
+-- Parameters
+-- ----------
+-- list_adr : string
+--     file path of the image list 
+--
+-- Returns
+-- -------
+-- image_list : table
+--     list of image paths
+-- 
+function neuralfeature.loadimagelist(list_adr)
+    f=io.open(list_adr, 'r');
+    -- local f = torch.DiskFile(list_adr, "r")
+    io.input(f);
+    image_list={};
+    while true do
+        local image_adr=io.read();  
+        if image_adr==nil then break end    
+        table.insert(image_list, image_adr);
+    end
+    
+    return image_list;
+end
---
+++ scripts/VGG16_feature.lua
@@ -0,0 +1,49 @@
+package.path = package.path .. ";../?.lua"
+
+require 'loadcaffe'
+require 'xlua'
+require 'optim'
+require 'image'
+require 'hdf5';
+
+require 'neuralfeature'
+
+-- prepare data
+
+prototxt='/home/profloo/Downloads/Caffe_Models/VGG_ILSVRC_16_layers_deploy.prototxt.txt'
+binary='/home/profloo/Downloads/Caffe_Models/VGG_ILSVRC_16_layers.caffemodel'
+
+path="/home/profloo/Desktop/Train_SFEW_Features/namelist/"
+
+local p = io.popen('find "'..path..'" -type f')  
+local dirlist = {}
+local i=1
+   for file in p:lines() do                          
+	dirlist[i]=file
+	i=i+1   
+   end
+print(dirlist)
+
+-- load as net
+net = loadcaffe.load(prototxt, binary)
+-- switch off dropout
+net:evaluate();
+
+for i = 1, #dirlist do
+	images = neuralfeature.loadimagelist(dirlist[i])
+	out, labels=neuralfeature.extract(net, images)
+	print(string.sub(dirlist[i],52))
+	out_save=torch.zeros(#images, 1000)
+
+	for i=1, #images do
+	    out_save[{ i, {} }]=out[i]    
+	end
+
+	print(out_save:size())
+
+	myFile=hdf5.open("/home/profloo/Desktop/Train_SFEW_Features/" .. string.sub(dirlist[i],52) .. ".h5", 'w');
+	myFile:write("/home/profloo/Desktop/Train_SFEW_Features/" .. string.sub(dirlist[i],52) .. ".h5", out_save);
+	myFile:close()
+	collectgarbage()
+end
+print("Done!")
